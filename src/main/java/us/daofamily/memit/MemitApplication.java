1552b84897a5520cd27495510561a1a558c4e3c5
Topher Dao <td.topherdao@gmail.com>
2019-11-19 19:12:21 -0500

    inital commit

---
+++ src/main/java/us/daofamily/memit/MemitApplication.java
@@ -0,0 +1,13 @@
+package us.daofamily.memit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/daofamily/memit/MemitApplicationTests.java
@@ -0,0 +1,13 @@
+package us.daofamily.memit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
